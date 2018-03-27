5e2efe7beaaf167653bcc9c58b69f3f3e7193876
Peter <pete.ragheb@gmail.com>
2018-03-27 18:10:40 +0200

    Added Lab5 Project

---
+++ Lab5_Step_Counter/app/src/main/java/com/example/peter/lab5_step_counter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.peter.lab5_step_counter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
